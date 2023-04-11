d3d01df08010961be2964039d75b51a3d360a5b9
loliconsama39 <113377766+loliconsama39@users.noreply.github.com>
2023-04-11 10:41:06 -0300

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -8,23 +8,41 @@ const categorias = [
         path: "/trending/all/week?API_KEY="+API_KEY+"&language=pt-br"
     },
     {
+        name: "netflixOriginal",
+        title: "Originais Netflix",
+        path: "/discover/tv/week?API_KEY="+API_KEY+"&language=pt-br"
+    },
+    {
+        name: "topRated",
+        title: "Populares",
+        path: "/movie/top_rated/API_KEY="+API_KEY+"&with_genres=99"
+    },
+    {
+        name: "comedy",
+        title: "Comedias",
+        path: "/discover/tv/API_KEY="+API_KEY+"&with_genres=99"
+    },
+    {
+        name: "romances",
+        title: "Romances",
+        path: "/discover/tv/API_KEY="+API_KEY+"&with_genres=99"
+    },
+    {
         name: "documentaries",
         title: "documentarios",
         path: "/discover/tv/API_KEY="+API_KEY+"&with_genres=99"
     }
-
 ]
 
-const getmovies = (categoria) => {
-    let url = ''
-
-    categorias.forEach((iten) => {
-        if(iten.name == categoria){
-            url = API_DNS + iten.path
-        }
-    });
+export default getmovies = async (path) => {
+    try {
+        let url = API_DNS + path
+        const response = fetch(url)
+        return (await response).json()
+    } catch (error) {
+        console.log("ERROR getMovies: ", error)
+    }
 
     return url;
 }
 
-export default getmovies;
